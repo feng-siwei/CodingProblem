5ba0e75ee321f753148ed393d31aa712d9fd6942
feng-siwei <fengsiwei93@qq.com>
2018-01-18 23:36:19 +0800

    剑指

---
+++ src/剑指offer/二维数组查找.java
@@ -0,0 +1,13 @@
+package 剑指offer;
+
+public class 二维数组查找 {
+	public static void main(String[] args) {
+		int [] [] matrix={{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+		
+	}
+	private boolean find(int[] [] matrix,int number) {
+		return false;
+		
+
+	}
+}
