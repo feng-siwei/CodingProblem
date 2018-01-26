150ebace623c084ac3ad1e091931373e39d96a8d
feng-siwei <fengsiwei93@qq.com>
2018-01-26 23:56:01 +0800

    剑指

--- src/剑指offer/双指针.java
+++ src/剑指offer/双指针.java
@@ -1,7 +1,12 @@
 package 剑指offer;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class 双指针 {
 	public static void main(String[] args) {
+		List<Integer> intList = new LinkedList<>();
+		
 		int arr ;
 		
 	}
