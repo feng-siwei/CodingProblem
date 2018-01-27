3b480b7f3f89b6ceecb1f54b07911d57dee6272d
feng-siwei <fengsiwei93@qq.com>
2018-01-27 23:52:03 +0800

    指针错误
    ConcurrentModificationException  异常

--- src/剑指offer/双指针.java
+++ src/剑指offer/双指针.java
@@ -2,12 +2,29 @@ package 剑指offer;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class 双指针 {
 	public static void main(String[] args) {
 		List<Integer> intList = new LinkedList<>();
+		ListIterator<Integer> iterator =intList.listIterator();
+		ListIterator<Integer> iterator2 = intList.listIterator();
 		
-		int arr ;
+		for (int i = 0; i < 100; i++) {
+			intList.add(i);		
+		}
+		
+//		for (int i = 0; i < 3; i++) {
+//			iterator.next();
+//		}
+		
+		while (iterator.hasNext()) {
+			Integer integer = (Integer) iterator.next();
+			System.out.println("这是第一个迭代器"+integer);
+			Integer integer2 = (Integer) iterator2.next();
+			System.out.println("这是第二个迭代器"+integer2);
+		}
+		
 		
 	}
 }
