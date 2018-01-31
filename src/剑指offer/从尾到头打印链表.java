c341d76e58ccc63e0a6e9d84a63934d5321a9325
feng-siwei <fengsiwei93@qq.com>
2018-02-01 03:17:07 +0800

    有关链表
    1.7链表取消了头指针

--- src/剑指offer/从尾到头打印链表.java
+++ src/剑指offer/从尾到头打印链表.java
@@ -17,7 +17,14 @@ public class 从尾到头打印链表 {
 	}
 	
 	private static void reversalPrint(List<String> list) {
-		ListIterator<String> iter = list.listIterator();
+		//很丑的方法,但1.7开始取消了头结点,使用两个指针指向头尾,而且构造方法中又优化,
+		// if (index < (size >> 1))从头开始,else从尾开始
+		ListIterator<String> iter = list.listIterator(list.size());
+		
+//		1.6有头指针可能成功具体需要看previous()方法的实现
+//		ListIterator<String> iter = list.listIterator();
+//		iter.previous();
+//		
 		while(iter.hasPrevious()){
 			System.out.println(iter.previous());
 		}
