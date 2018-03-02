50799b9c5f1115897c07a45b975b5816cc5c2b62
feng-siwei <fengsiwei93@qq.com>
2018-03-02 23:54:02 +0800

    二进制多少1方法2
    普通方法补充
    将数字与每次<<1位的1做且运算,不为0就代表有这个数字

--- src/剑指offer/二进制中有多少个1.java
+++ src/剑指offer/二进制中有多少个1.java
@@ -10,8 +10,21 @@ public class 二进制中有多少个1 {
 			int count = 0;
 			while (n!=0) {
 				count++;
+				//当n与n-1且运算,会消除最右侧的1
 				n = n & (n-1);
 			}
+			
+			//普通方法
+//			int flag = 1;
+//			//int类型为4字节,一字节8位,需要循环32次
+//			while (flag != 0) {
+//				if ((n&flag) != 0) {
+//					count++;
+//				}
+//				flag = flag<<1;
+//				System.out.printf("flag的值为%d %n", flag);
+//			}
+			
 			System.out.println(count);
 		}	
 	}
