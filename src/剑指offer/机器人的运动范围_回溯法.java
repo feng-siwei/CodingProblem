e6d0f93c08d32e77a3a381a905074a6685c3332f
feng-siwei <fengsiwei93@qq.com>
2018-03-04 01:03:48 +0800

    机器人行走矩形图
    回溯法实现

--- src/剑指offer/机器人的运动范围_回溯法.java
+++ src/剑指offer/机器人的运动范围_回溯法.java
@@ -3,9 +3,9 @@ package 剑指offer;
 public class 机器人的运动范围_回溯法 {
 	public static void main(String[] args) {
 		//临界条件
-		int threshold = 15;
+		int threshold = 18;
 		int rows = 200;
-		int cols = 1;
+		int cols = 200;
 		int count = movingCount(threshold,rows,cols);
 		System.out.printf("最后进入格子数为%d %n",count);
 	}
@@ -14,10 +14,23 @@ public class 机器人的运动范围_回溯法 {
 		if (threshold<0||rows<0||cols<0) {
 			return 0 ;
 		}
-		boolean [] visited = new boolean[(rows+1)*(cols+1)];
+		boolean [] visited = new boolean[rows*cols];
 		
 		int count = movingCountCore(threshold,rows,cols,0,0,visited);
 		
+		
+		//打印矩形看看
+		for (int i = 0; i < rows; i++) {
+			System.out.print(i);
+			for (int j = 0; j < cols; j++) {
+				if (visited[i*cols+j]) {
+					System.out.print("●");
+				}else {
+					System.out.print("○");
+				}
+			}
+			System.out.print("\n");
+		}
 		return count;
 	}
 	//核心方法用于递归,查看是否可以进入,然后递归上下左右
@@ -38,7 +51,8 @@ public class 机器人的运动范围_回溯法 {
 	//检查格子是否可以进入
 	private static boolean check(int threshold, int rows, int cols, int row,
 			int col, boolean[] visited) {
-		if (rows>=0 && cols>=0&&row<=rows&&col<=cols 
+		
+		if (row>=0 && col>=0&&row<rows&&col<cols 
 				&&getDigitSum(row)+getDigitSum(col)<threshold
 				&&!visited[row*cols+col]) {
 			return true;
@@ -48,7 +62,7 @@ public class 机器人的运动范围_回溯法 {
 	//计算位数之和
 	private static int getDigitSum(int number) {
 		int sum = 0;
-		while (number < 0 ) {
+		while (number > 0 ) {
 			sum += number%10;
 			number = number/10;
 		}
